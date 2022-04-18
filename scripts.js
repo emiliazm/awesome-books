04b6b90d6025b9ec0e8df4282c0c8d2f56b0ee54
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-18 15:58:32 -0500

    Add ids to books Remove books by id Refactor script.js

--- scripts.js
+++ scripts.js
@@ -1,79 +1,35 @@
 let books = [];
 
 // STORAGE
-function storageAvailable(type) {
-  let storage;
-  try {
-    storage = window[type];
-    const x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  } catch (e) {
-    return (
-      e instanceof DOMException
-      // everything except Firefox
-      && (e.code === 22
-        // Firefox
-        || e.code === 1014
-        // test name field too, because code might not be present
-        // everything except Firefox
-        || e.name === 'QuotaExceededError'
-        // Firefox
-        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-      // acknowledge QuotaExceededError only if there's something already stored
-      && storage
-      && storage.length !== 0
-    );
-  }
+
+function getNewId() {
+  const id = parseInt(localStorage.getItem('bookId') || '0', 10) + 1;
+  localStorage.setItem('bookId', id);
+  return id;
 }
 
-const loadBooks = () => {
+function loadBooks() {
   books = JSON.parse(localStorage.getItem('books'));
-};
-
-const saveBooks = (books) => {
-  localStorage.setItem('books', JSON.stringify(books));
-};
-
-function add(title, author) {
-  const book = {
-    title, author,
-  };
-  books.push(book);
-  saveBooks(books);
-}
-
-function addBook() {
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  add(title, author);
-  showBooks();
-}
-
-function remove(title, author) {
-  books = books.filter((book) => book.title !== title || book.author !== author);
-  saveBooks(books);
-  showBooks()
 }
 
 function fillValues(card, book) {
   card.querySelector('.title').textContent = book.title;
   card.querySelector('.author').textContent = book.author;
   card.querySelector('button').addEventListener('click', () => {
-    remove(book.title, book.author);
+    // eslint-disable-next-line no-use-before-define
+    remove(book.id);
   });
 }
 
 function clearBookList(container) {
-  container.innerHTML = "";
+  container.innerHTML = '';
 }
 
 function showBooks() {
   loadBooks();
   const container = document.querySelector('.book-list');
   const template = document.querySelector('.book');
-  template.style.display='block';
+  template.style.display = 'block';
   clearBookList(container);
   books.forEach((book) => {
     const card = template.cloneNode(true);
@@ -83,3 +39,27 @@ function showBooks() {
   template.style.display = 'none';
 }
 
+function saveBooks(books) {
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function add(title, author) {
+  const id = getNewId();
+  const book = { id, title, author };
+  books.push(book);
+  saveBooks(books);
+}
+
+// eslint-disable-next-line no-unused-vars
+function addBook() {
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  add(title, author);
+  showBooks();
+}
+
+function remove(id) {
+  books = books.filter((book) => book.id !== id);
+  saveBooks(books);
+  showBooks();
+}
