be6768894af4afc6143083089029ba8a5c8fea49
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-18 15:32:25 -0500

    Add local storage

--- scripts.js
+++ scripts.js
@@ -1,9 +1,47 @@
 let books = [];
+
+// STORAGE
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException
+      // everything except Firefox
+      && (e.code === 22
+        // Firefox
+        || e.code === 1014
+        // test name field too, because code might not be present
+        // everything except Firefox
+        || e.name === 'QuotaExceededError'
+        // Firefox
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      // acknowledge QuotaExceededError only if there's something already stored
+      && storage
+      && storage.length !== 0
+    );
+  }
+}
+
+const loadBooks = () => {
+  books = JSON.parse(localStorage.getItem('books'));
+};
+
+const saveBooks = (books) => {
+  localStorage.setItem('books', JSON.stringify(books));
+};
+
 function add(title, author) {
   const book = {
     title, author,
   };
   books.push(book);
+  saveBooks(books);
 }
 
 function addBook() {
@@ -15,6 +53,7 @@ function addBook() {
 
 function remove(title, author) {
   books = books.filter((book) => book.title !== title || book.author !== author);
+  saveBooks(books);
   showBooks()
 }
 
@@ -31,6 +70,7 @@ function clearBookList(container) {
 }
 
 function showBooks() {
+  loadBooks();
   const container = document.querySelector('.book-list');
   const template = document.querySelector('.book');
   template.style.display='block';
@@ -42,3 +82,4 @@ function showBooks() {
   });
   template.style.display = 'none';
 }
+
