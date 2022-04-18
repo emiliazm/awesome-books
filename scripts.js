019d01fa56f9a7ff4fbbb2d8ec4fccbe96e5af6e
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-04-18 21:44:46 +0200

    Deploy all books

--- scripts.js
+++ scripts.js
@@ -1,7 +1,36 @@
-const books = [];
+let books = [];
 function add(title, author) {
   const book = {
     title, author,
   };
   books.push(book);
 }
+function remove(title, author) {
+  books = books.filter((book) => book.title !== title || book.author !== author);
+  console.log(books);
+}
+function fillValues(card, book) {
+  card.querySelector('.title').textContent = book.title;
+  card.querySelector('.author').textContent = book.author;
+  card.querySelector('button').addEventListener('click', () => {
+    remove(book.title, book.author);
+  });
+}
+function showBooks() {
+  const container = document.querySelector('.book-list');
+  const template = document.querySelector('.book');
+  books.forEach((book) => {
+    const card = template.cloneNode(true);
+    fillValues(card, book);
+    container.appendChild(card);
+  });
+  template.remove();
+}
+
+// test
+add('cien años', 'garcia');
+add('rayuela', 'cortazar');
+console.log(books);
+remove('rayuela', 'cortazar');
+console.log(books);
+showBooks();
