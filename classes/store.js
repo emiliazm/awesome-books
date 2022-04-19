e0be501372c0957ff61fee24ff22c19ff636c9e3
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-19 12:07:07 -0500

    Return Books[] instead of  Objects[]

--- classes/store.js
+++ classes/store.js
@@ -1,10 +1,15 @@
+import Book from './book.js';
+
 export default  class Store {
   static #idKey = 'idKey';
 
   static #booksKey = 'books';
 
   static get books() {
-    return JSON.parse(localStorage.getItem(Store.#booksKey) || '[]');
+    return JSON.parse(localStorage.getItem(Store.#booksKey) || '[]', (key, value) => {
+      if (key !== '' && !Number.isNaN(parseInt(key))) return new Book(value.id, value.title, value.author);
+      return value;
+    });
   }
 
   static set books(books) {
