011b2f1cf55f57a0a7c8bb58b42550b4ecd2e097
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-04-19 17:35:55 +0200

    Add BookCollection class

---
+++ classes/book-collection.js
@@ -0,0 +1,29 @@
+import Book from './book.js';
+import Store from './store.js';
+
+export default class BookCollection {
+  static get books() {
+    return Store.books;
+  }
+
+  static get nextId() {
+    return Store.nextId;
+  }
+
+  static add(book) {
+    const { books } = Store;
+    books.push(book);
+    Store.books = books;
+    return book;
+  }
+
+  static addBook(title, author) {
+    const newBook = new Book(title, author);
+    return BookCollection.add(newBook);
+  }
+
+  static remove(id) {
+    const books = BookCollection.books.filter((book) => book.id !== id);
+    Store.books = books;
+  }
+}
