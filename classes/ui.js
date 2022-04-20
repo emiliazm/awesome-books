7d53447c4691fdb0321281cf5f94d172e107c924
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-20 09:49:42 -0500

    Refactor scripts to UI class

--- classes/ui.js
+++ classes/ui.js
@@ -5,7 +5,17 @@ export default class UI {
 
   static #bookElement = document.querySelector('.book');
 
-  static addBook(book) {
+  static #titleElement = document.querySelector('#title');
+
+  static #authorElement = document.querySelector('#author');
+
+  static #bookListMenu = document.querySelector('#bookList');
+
+  static #bookFormMenu = document.querySelector('#bookForm');
+
+  static #contactMenu = document.querySelector('#contact');
+
+  static displayBook(book) {
     const card = UI.#bookElement.cloneNode(true);
     card.querySelector('.title-author').textContent = book.toString();
     card.querySelector('button').addEventListener('click', (event) => UI.removeBook(event, book));
@@ -20,6 +30,28 @@ export default class UI {
   }
 
   static displayBooks() {
-    BookCollection.books.forEach((book) => this.addBook(book));
+    BookCollection.books.forEach((book) => this.displayBook(book));
+  }
+
+  static addNewBook(event) {
+    const title = UI.#titleElement.value.trim();
+    const author = UI.#authorElement.value.trim();
+
+    if (title === '' || author === '') {
+      event.preventDefault();
+    } else {
+      const book = BookCollection.addBook(title, author);
+      UI.displayBook(book);
+      UI.#titleElement.value = '';
+      UI.#authorElement.value = '';
+    }
+  }
+
+  static showSection(id) {
+    if (id !== '') {
+      UI.#bookListMenu.classList.toggle('hidden', (id !== 'goList'));
+      UI.#bookFormMenu.classList.toggle('hidden', (id !== 'goForm'));
+      UI.#contactMenu.classList.toggle('hidden', (id !== 'goCont'));
+    }
   }
 }
--- scripts.js
+++ scripts.js
@@ -1,44 +1,7 @@
-import BookCollection from './classes/book-collection.js';
 import UI from './classes/ui.js';
 
-document.addEventListener('DOMContentLoaded', UI.displayBooks());
-document.querySelector('#addButton').addEventListener('click', (e) => {
-  const titleElement = document.querySelector('#title');
-  const authorElement = document.querySelector('#author');
-  const title = titleElement.value.trim();
-  const author = authorElement.value.trim();
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-  if (title === '' || author === '') {
-    e.preventDefault();
-  } else {
-    const book = BookCollection.addBook(title, author);
-    UI.addBook(book);
-    titleElement.value = '';
-    authorElement.value = '';
-  }
-});
+document.querySelector('#addButton').addEventListener('click', (event) => UI.addNewBook(event));
 
-const listLi = document.querySelector('.list-li');
-const sideBorder = document.querySelector('.sideBorder');
-const contactLi = document.querySelector('.contact-li');
-const bookList = document.querySelector('#bookList');
-const bookForm = document.querySelector('#bookForm');
-const contact = document.querySelector('#contact');
-
-listLi.addEventListener('click', () => {
-  bookList.classList.remove('hidden');
-  bookForm.classList.add('hidden');
-  contact.classList.add('hidden');
-});
-
-sideBorder.addEventListener('click', () => {
-  bookList.classList.add('hidden');
-  bookForm.classList.remove('hidden');
-  contact.classList.add('hidden');
-});
-
-contactLi.addEventListener('click', () => {
-  bookList.classList.add('hidden');
-  bookForm.classList.add('hidden');
-  contact.classList.remove('hidden');
-});
+document.querySelector('.menuList').addEventListener('click', (event) => UI.showSection(event.target.id));
